60347d88bf5543aab58707ee6af10f8f175fa2ca
Laksh Goel <lakshgoel541@gmail.com>
2025-08-27 18:43:46 +0530

    socket connection by client and message sending logic written in client.cpp

--- part1/client.cpp
+++ part1/client.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sys/socket.h> //core socket API declarations
+#include <arpa/inet.h> //address conversion functions
+#include <unistd.h> //low-level system call interface
+#include <cstring> //string manipulation functions
 
 using namespace std;
 
@@ -43,3 +47,60 @@ map<string, string> parse_config(const string filename){
     return config;
 }
 
+
+int main() {
+    // get server configuration
+    string filename = "config.json";
+    map<string, string> config;
+    try{
+        config = parse_config(filename);
+    } catch (const exception& e) {
+        cerr << "Error: Could not parse " << filename << ": " << e.what() << endl;
+        return 1;
+    }
+
+    // connect to server
+    const string server_ip = config["server_ip"];
+    const int server_port = stoi(config["server_port"]);
+
+    //network connection endpoint
+    int sock = socket(AF_INET, SOCK_STREAM, 0); //ipv4, tcp
+    if(sock == -1){
+        cerr << "Error: Could not create socket" << endl;
+        return 1;
+    }
+
+    //create server address structure
+    struct sockaddr_in serv_addr; //sockaddr_in is specefic version for IPv4 of general sockaddr structure
+    serv_addr.sin_family = AF_INET; //ipv4
+    //Network protocol: Big-endian (most significant byte first)
+    serv_addr.sin_port = htons(server_port); //convert to network byte order
+
+    if (inet_pton(AF_INET, server_ip.c_str(), &serv_addr.sin_addr) <= 0) { //IPv4, string form in null terminated C style, binary form
+        // 1->success, 0->invalid address, -1->error
+        cerr << "Invalid address / Address not supported" << endl;
+        return 1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) { 
+        //socket descriptor, server address structure(type cast to generic type)(C style cast recommended), size of the structure
+        //Does a three-way handshake SYN ->, SYN-ACK <-, ACK ->
+        //returns 0 on success, -1 on error
+        cerr << "Connection Failed" << endl;
+        return 1;
+    }
+    cout << "Connected to server " << server_ip << " on port " << server_port << endl;
+
+    //send and receive data
+    string k = config["k"];
+    string p = config["p"];
+    const string message = k + "," + p; //message to be sent
+    send(sock, message.c_str(), message.length(), 0); //socket descriptor, message in C style, length of the message, flags(0 for no special options)
+    cout << "Message sent: " << message << endl;
+
+    
+
+    close(sock);
+
+    return 0;
+}
