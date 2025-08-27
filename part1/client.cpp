92da614267061a6e3ae98560eb1f7ed4034f6938
Laksh Goel <lakshgoel541@gmail.com>
2025-08-27 17:11:11 +0530

    Edited Parser

--- part1/client.cpp
+++ part1/client.cpp
@@ -21,9 +21,23 @@ map<string, string> parse_config(const string filename){
         //itreate the line
         size_t pos = line.find(':');
         if(pos != string::npos){
-            string key = line.substr(0, pos); //pos not included
-            string value = line.substr(pos + 2); //+2 to skip ": "
-            config[key] = value;            
+            string key = line.substr(0, pos); //pos i.e. ':' not included
+            string value = line.substr(pos + 1);
+
+            //trim starting and trailing whitespaces and trailing commas
+            key.erase(0, key.find_first_not_of(" "));
+            key.erase(key.find_last_not_of(" ,") + 1);
+            value.erase(0, value.find_first_not_of(" "));
+            value.erase(value.find_last_not_of(" ,") + 1);
+
+            //trim quotes if exist
+            if (!key.empty() && key.front() == '"' && key.back() == '"') {
+                key = key.substr(1, key.length() - 2);
+            }
+            if (!value.empty() && value.front() == '"' && value.back() == '"') {
+                value = value.substr(1, value.length() - 2);
+            }
+            config[key] = value;
         }
     }
     return config;
